4124657cf16c6f582f35f0c8ca9cc3855ba3742a
KHeal <kheal@uw.edu>
2020-10-16 15:05:22 -0700

    plotted elutes and crudes together

--- Figures/Rcode/Lipid_Exploratory_Plots.R
+++ Figures/Rcode/Lipid_Exploratory_Plots.R
@@ -4,16 +4,22 @@ library(cowplot)
 theme_set(theme_cowplot())
 library(stats)
 
-# Files to examine
+# Files to examine-----
 files <- c("RawDat/20200116_icapdata_firstLipidRun/2020_01_16_Heal_AsLipidsRP_3.csv", 
            "RawDat/20200116_icapdata_firstLipidRun/2020_01_16_Heal_AsLipidsRP_4.csv",
            "RawDat/20200116_icapdata_firstLipidRun/2020_01_16_Heal_AsLipidsRP_5.csv")
 
-samps_from_files1 <- c("ALOHA_crude", "ALOHA_elute", "ALOHAPrefilter_crude", "ALOHAPrefilter_elute",
-                       "PS107_crude", "PS107_elute_1", "ALOHABlank_crude_1")
+samps_from_files1_crude <- c("ALOHA_crude","ALOHAPrefilter_crude", 
+                             "PS107_crude")
 
-samps_from_files2 <- c("PS2_crude", "PS2_elute", "PS3_crude", "PS3_elute",
-                       "BATS07_crude", "BATS07_elute", "BATS03_crude")
+samps_from_files1_elute <- c( "ALOHA_elute",  "ALOHAPrefilter_elute",
+                       "PS107_elute_1", "ALOHABlank_crude_1")
+
+samps_from_files2_crude <- c("PS2_crude","PS3_crude",
+                       "BATS07_crude")
+
+samps_from_files2_elute <- c( "PS2_elute", "PS3_elute",
+                        "BATS07_elute")
 
 # Plot out files from first fileset -----
 icapdat1 <- read_delim(files[1], 
@@ -21,27 +27,41 @@ icapdat1 <- read_delim(files[1],
                        skip = 1)
 plots <- list()
 
-for (i in 1:length(samps_from_files1)){
-dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files1[i]) %>%
+for (i in 1:length(samps_from_files1_crude)){
+dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files1_crude[i]) %>%
   rename(scan = X2, analyte  = X3, type = X4)
-  
 dat_as <- dat_all %>%
   filter(analyte == "75As" & type == "Y") %>%
-  rename(As.intensity = samps_from_files1[i]) %>% select(-analyte, -type)
-
+  rename(As.intensity = samps_from_files1_crude[i]) %>% select(-analyte, -type)
 dat_time <- dat_all %>%
   filter(analyte == "75As" & type == "Time") %>%
-  rename(As.time = samps_from_files1[i]) %>% select(-analyte, -type)
-
+  rename(As.time = samps_from_files1_crude[i]) %>% select(-analyte, -type)
 dat_time_as <- left_join(dat_as, dat_time, by = "scan")  %>%
   filter(As.time > 200 & As.time < 2000) %>% select(-scan)
-
-dat_to_plot <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
+dat_to_plot_crude <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
                        "Intensity" = runmed(dat_time_as$As.intensity, k = 7))
 
-plots[[i]] <- ggplot(data = dat_to_plot, aes(x = Time,  y = Intensity)) +
+dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files1_elute[i]) %>%
+  rename(scan = X2, analyte  = X3, type = X4)
+dat_as <- dat_all %>%
+  filter(analyte == "75As" & type == "Y") %>%
+  rename(As.intensity = samps_from_files1_elute[i]) %>% select(-analyte, -type)
+dat_time <- dat_all %>%
+  filter(analyte == "75As" & type == "Time") %>%
+  rename(As.time = samps_from_files1_elute[i]) %>% select(-analyte, -type)
+dat_time_as <- left_join(dat_as, dat_time, by = "scan")  %>%
+  filter(As.time > 200 & As.time < 2000) %>% select(-scan)
+dat_to_plot_elute <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
+                            "Intensity" = runmed(dat_time_as$As.intensity, k = 7))
+
+dat_to_plot <- dat_to_plot_elute %>% 
+  mutate(ID = samps_from_files1_elute[i]) %>%
+  bind_rows(dat_to_plot_crude
+            %>% mutate(ID = samps_from_files1_crude[i]))
+
+plots[[i]] <- ggplot(data = dat_to_plot, aes(x = Time,  y = Intensity, color = ID)) +
   geom_line() + 
-  ggtitle(samps_from_files1[i])
+    ggtitle(samps_from_files1_crude[i])
 }
 
 
@@ -52,38 +72,49 @@ icapdat1 <- read_delim(files[2],
                        skip = 1)
 plots_2 <- list()
 
-for (i in 1:length(samps_from_files2)){
-  dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files2[i]) %>%
+for (i in 1:length(samps_from_files2_crude)){
+  dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files2_crude[i]) %>%
     rename(scan = X2, analyte  = X3, type = X4)
-  
   dat_as <- dat_all %>%
     filter(analyte == "75As" & type == "Y") %>%
-    rename(As.intensity = samps_from_files2[i]) %>% select(-analyte, -type)
-  
+    rename(As.intensity = samps_from_files2_crude[i]) %>% select(-analyte, -type)
   dat_time <- dat_all %>%
     filter(analyte == "75As" & type == "Time") %>%
-    rename(As.time = samps_from_files2[i]) %>% select(-analyte, -type)
-  
+    rename(As.time = samps_from_files2_crude[i]) %>% select(-analyte, -type)
   dat_time_as <- left_join(dat_as, dat_time, by = "scan")  %>%
     filter(As.time > 200 & As.time < 2000) %>% select(-scan)
+  dat_to_plot_crude <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
+                              "Intensity" = runmed(dat_time_as$As.intensity, k = 7))
   
-  dat_to_plot <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
-                        "Intensity" = runmed(dat_time_as$As.intensity, k = 7))
+  dat_all <- icapdat1 %>% select(X2, X3, X4, samps_from_files2_elute[i]) %>%
+    rename(scan = X2, analyte  = X3, type = X4)
+  dat_as <- dat_all %>%
+    filter(analyte == "75As" & type == "Y") %>%
+    rename(As.intensity = samps_from_files2_elute[i]) %>% select(-analyte, -type)
+  dat_time <- dat_all %>%
+    filter(analyte == "75As" & type == "Time") %>%
+    rename(As.time = samps_from_files2_elute[i]) %>% select(-analyte, -type)
+  dat_time_as <- left_join(dat_as, dat_time, by = "scan")  %>%
+    filter(As.time > 200 & As.time < 2000) %>% select(-scan)
+  dat_to_plot_elute <- tibble("Time" = runmed(dat_time_as$As.time, k = 7),
+                              "Intensity" = runmed(dat_time_as$As.intensity, k = 7))
   
-  plots_2[[i]] <- ggplot(data = dat_to_plot, aes(x = Time,  y = Intensity)) +
+  dat_to_plot <- dat_to_plot_elute %>% 
+    mutate(ID = samps_from_files2_elute[i]) %>%
+    bind_rows(dat_to_plot_crude
+              %>% mutate(ID = samps_from_files2_crude[i]))
+  
+  plots_2[[i]] <- ggplot(data = dat_to_plot, aes(x = Time,  y = Intensity, color = ID)) +
     geom_line() + 
-    ggtitle(samps_from_files2[i])
+    ggtitle(samps_from_files2_crude[i])
 }
 
+
+
 # grid out the plots
-plot_save_1 <- plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], ncol = 1)
-plot_save_2 <- plot_grid(plots_2[[1]], plots_2[[2]], plots_2[[3]], plots_2[[4]], plots_2[[5]], plots_2[[6]], ncol = 1)
+plot_save_1 <- plot_grid(plots[[1]], plots[[2]], plots[[3]], ncol = 1)
+plot_save_2 <- plot_grid(plots_2[[1]], plots_2[[2]], plots_2[[3]], ncol = 1)
 
-# crudes only
-plot_crudes <- plot_grid(plots_2[[7]], plots[[1]], plots[[5]], plots_2[[1]], plots_2[[3]], plots_2[[5]], ncol = 1)
-plot_crudes
+save_plot("Figures/Preliminary/As_traces_1.pdf", plot_save_1, base_width = 8, base_height = 12, units = "in")
+save_plot("Figures/Preliminary/As_traces_2.pdf", plot_save_2, base_width = 8, base_height = 12, units = "in")
 
-save_plot("Figures/Preliminary/As_traces.pdf", plot_crudes, base_width = 8, base_height = 12, units = "in")
-
-
-# Print out crudes only
