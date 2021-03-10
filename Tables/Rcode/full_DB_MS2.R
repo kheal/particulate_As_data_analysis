75453e9930e8b05072c4a1d265b268dc73d94247
KHeal <kheal@uw.edu>
2021-03-10 15:08:54 -0800

    ms1 and ms2 database output

--- Tables/Rcode/full_DB_MS2.R
+++ Tables/Rcode/full_DB_MS2.R
@@ -1,11 +1,18 @@
-library(here)
 library(tidyverse)
+library(Rdisop)
 
 db <- read_csv("MetaData/AsLipidDatabase/full_MS1db.csv")
-#db_ms2 <- read_csv("MetaData/AsLipidDatabase/MS2_Fragments.csv")
+db_ms2 <- read_csv("MetaData/AsLipidDatabase/MS2_Fragments.csv")
 
-db_clean <- db %>%
-  select(LipidClass:mz) %>%
+db_ms2_2 <- db_ms2 %>%
+  filter(str_detect(Formula, "As")) %>%
+  filter(LipidClass %in% db$LipidClass) %>%
+  mutate(mz = sapply(Formula, 
+                   function(x)getMolecule(x)$exactmass)) 
+
+
+db_clean <- db_ms2_2 %>%
+  select(LipidClass, Formula, mz) %>%
   rename(`Lipid Class (short)` = LipidClass,
          `Lipid ID (ms1 only)` = Lipid_Name,
          `SN1 tail length` = tail_length1,
@@ -15,4 +22,4 @@ db_clean <- db %>%
          `Empirical Formula (in ionic state)` = EmpiricalFormula,
          `m/z` = mz)
 
-write_csv(db_clean, "Tables/ManuscriptReady/SuppTables/DB_summary_full_MS1.csv")
+#write_csv(db_clean, "Tables/ManuscriptReady/SuppTables/DB_summary_full_MS1.csv")
